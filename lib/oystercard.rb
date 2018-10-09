1733d1efaa52d0da076bf8d4ec984309ae307485
Max <max.francis2000@gmail.com>
2018-10-09 14:00:35 +0000

    adding features and tests

---
+++ lib/oystercard.rb
@@ -0,0 +1,32 @@
+class Oystercard
+  
+  MAXIMUM_BALANCE = 90
+  
+  attr_reader :balance, :MAXIMUM_BALANCE
+  
+  def initialize
+    @balance = 0
+  end
+  
+  def top_up(amount)
+    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+  
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    false
+  end
+
+  def touch_in
+    in_journey? == true
+  end
+  
+  def touch_out
+    in_journey? == false
+  end
+  
+end
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,50 @@
+require 'oystercard'
+
+describe Oystercard do
+  it 'oyster card displays balance' do 
+    expect(subject.balance).to eq(0)
+  end
+  
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+      
+    it 'top up the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+      
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up maximum_balance
+      raise_error(RuntimeError, "Maximum balance of #{maximum_balance} exceeded")
+    end
+      
+  describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+    
+    it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct 3}.to change{ subject.balance }.by (-3)
+    end
+  end
+  
+    describe '#in_journey' do
+    
+      it 'is initially not in a journey' do
+        expect(subject).not_to be_in_journey
+      end
+    
+      it "can touch in" do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+    
+      it "can touch out" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
+      end
+    
+    end
+  
+  end
+end
