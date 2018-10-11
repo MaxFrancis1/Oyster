407824ab28c23d884838141acf5f9a8331118f29
Max <max.francis2000@gmail.com>
2018-10-11 12:44:01 +0000

    completed a few more stages, Now doing stage 13

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,9 @@
 class Oystercard
   
   MAXIMUM_BALANCE = 90
+  MINIMUM_CHARGE = 1
   
-  attr_reader :balance, :MAXIMUM_BALANCE, :in_journey
+  attr_reader :balance, :MAXIMUM_BALANCE, :in_journey, :entry_station, :exit_station
   
   def initialize
     @balance = 0
@@ -19,16 +20,19 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
-
-  def touch_in
+  
+  def touch_in(station)
+    @entry_station = station
     fail "Insufficient balance to touch in" if balance < 1
     @in_journey = true
   end
   
-  def touch_out
-    @in_journey = false
+  def touch_out(exit_station)
+    @exit_station = exit_station
+    deduct(MINIMUM_CHARGE)
+    @entry_station = nil
   end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,9 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
   
+   let(:entry_station) { double :station }
+   let(:exit_station) { double :station }
+  
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
       
@@ -25,6 +28,10 @@ describe Oystercard do
     subject.top_up(20)
     expect{ subject.deduct 3}.to change{ subject.balance }.by (-3)
     end
+    
+    it 'will deduct money from card after touch in' do
+        expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+    end
   end
   
     describe '#in_journey' do
@@ -34,23 +41,39 @@ describe Oystercard do
       end
     
       it 'will not touch in if below minimum balance' do
-        expect{ subject.touch_in }.to raise_error "Insufficient balance to touch in"
+        expect{ subject.touch_in(station) }.to raise_error "Insufficient balance to touch in"
       end
     
       it "can touch in" do
         subject.top_up(5)
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject).to be_in_journey
       end
     
       it "can touch out" do
         subject.top_up(5)
-        subject.touch_in
-        subject.touch_out
+        subject.touch_in(station)
+        subject.touch_out(exit_station)
         expect(subject).not_to be_in_journey
       end
-    
+        
+        let(:station){ double :station }
+
+        it 'stores the entry station' do
+          subject.top_up(5)
+          subject.touch_in(station)
+          expect(subject.entry_station).to eq station
+        end
+        
+       
+
+        it 'stores exit station' do
+          subject.top_up(5)
+          subject.touch_in(entry_station)
+          subject.touch_out(exit_station)
+          expect(subject.exit_station).to eq exit_station
+        end
+        
     end
-  
   end
 end
